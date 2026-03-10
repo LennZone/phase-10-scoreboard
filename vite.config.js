3dbb71eb2344e9027e412fedbb4c25a3c77d8952
Lennart Novak <lennart.novak@schauinsland-reisen.de>
2026-03-10 20:31:06 +0100

    fix (deployment): change url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/phase-10-scoreboard/' : '/',
+  base: mode === 'production' ? '/phase-10-score-tracker/' : '/',
   server: {
     host: '0.0.0.0',
     port: 5176,
